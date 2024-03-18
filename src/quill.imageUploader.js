ffe6fea1ed1b0f6840e074d130796d11c53bc58e
Lucas Veronezzi <lucasveronezzi.ti@gmail.com>
2024-03-18 13:06:20 +1300

    fix(bug): upload more than one image at the same time

--- src/quill.imageUploader.js
+++ src/quill.imageUploader.js
@@ -1,209 +1,230 @@
-import LoadingImage from "./blots/image.js";
+import LoadingImage from './blots/image.js';
 
 class ImageUploader {
-    constructor(quill, options) {
-        this.quill = quill;
-        this.options = options;
-        this.range = null;
-        this.placeholderDelta = null;
-        this.placeholderLeaf = null;
+  constructor(quill, options) {
+    this.quill = quill;
+    this.options = options;
+    this.range = null;
+    this.placeholderDelta = null;
+    this.placeholderLeafs = new Map();
 
-        if (typeof this.options.upload !== "function")
-            console.warn(
-                "[Missing config] upload function that returns a promise is required"
-            );
-
-        var toolbar = this.quill.getModule("toolbar");
-        if (toolbar) {
-            toolbar.addHandler("image", this.selectLocalImage.bind(this));
-        }
-
-        this.handleDrop = this.handleDrop.bind(this);
-        this.handlePaste = this.handlePaste.bind(this);
-
-        this.quill.root.addEventListener("drop", this.handleDrop, false);
-        this.quill.root.addEventListener("paste", this.handlePaste, false);
+    if (typeof this.options.upload !== 'function') {
+      console.warn(
+        '[Missing config] upload function that returns a promise is required',
+      );
     }
 
-    selectLocalImage() {
+    const toolbar = this.quill.getModule('toolbar');
+    if (toolbar) {
+      toolbar.addHandler('image', this.selectLocalImage.bind(this));
+    }
+
+    this.handleDrop = this.handleDrop.bind(this);
+    this.handlePaste = this.handlePaste.bind(this);
+
+    this.quill.root.addEventListener('drop', this.handleDrop, false);
+    this.quill.root.addEventListener('paste', this.handlePaste, false);
+  }
+
+  selectLocalImage() {
+    this.quill.focus();
+    this.range = this.quill.getSelection();
+    this.fileHolder = document.createElement('input');
+    this.fileHolder.setAttribute('type', 'file');
+    this.fileHolder.setAttribute('accept', 'image/*');
+    this.fileHolder.setAttribute('style', 'visibility:hidden');
+
+    this.fileHolder.onchange = this.fileChanged.bind(this);
+
+    document.body.appendChild(this.fileHolder);
+
+    this.fileHolder.click();
+
+    window.requestAnimationFrame(() => {
+      document.body.removeChild(this.fileHolder);
+    });
+  }
+
+  handleDrop(evt) {
+    if (
+      evt.dataTransfer
+            && evt.dataTransfer.files
+            && evt.dataTransfer.files.length
+    ) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      if (document.caretRangeFromPoint) {
+        const selection = document.getSelection();
+        const range = document.caretRangeFromPoint(evt.clientX, evt.clientY);
+        if (selection && range) {
+          selection.setBaseAndExtent(
+            range.startContainer,
+            range.startOffset,
+            range.startContainer,
+            range.startOffset,
+          );
+        }
+      } else {
+        const selection = document.getSelection();
+        const range = document.caretPositionFromPoint(evt.clientX, evt.clientY);
+        if (selection && range) {
+          selection.setBaseAndExtent(
+            range.offsetNode,
+            range.offset,
+            range.offsetNode,
+            range.offset,
+          );
+        }
+      }
+
+      this.quill.focus();
+      this.range = this.quill.getSelection();
+      const file = evt.dataTransfer.files[0];
+
+      setTimeout(() => {
         this.quill.focus();
         this.range = this.quill.getSelection();
-        this.fileHolder = document.createElement("input");
-        this.fileHolder.setAttribute("type", "file");
-        this.fileHolder.setAttribute("accept", "image/*");
-        this.fileHolder.setAttribute("style", "visibility:hidden");
-
-        this.fileHolder.onchange = this.fileChanged.bind(this);
-
-        document.body.appendChild(this.fileHolder);
-
-        this.fileHolder.click();
-
-        window.requestAnimationFrame(() => {
-            document.body.removeChild(this.fileHolder);
-        });
+        this.readAndUploadFile(file);
+      }, 0);
     }
+  }
 
-    handleDrop(evt) {
-        if (
-            evt.dataTransfer &&
-            evt.dataTransfer.files &&
-            evt.dataTransfer.files.length
-        ) {
-            evt.stopPropagation();
-            evt.preventDefault();
-            if (document.caretRangeFromPoint) {
-                const selection = document.getSelection();
-                const range = document.caretRangeFromPoint(evt.clientX, evt.clientY);
-                if (selection && range) {
-                    selection.setBaseAndExtent(
-                        range.startContainer,
-                        range.startOffset,
-                        range.startContainer,
-                        range.startOffset
-                    );
-                }
-            } else {
-                const selection = document.getSelection();
-                const range = document.caretPositionFromPoint(evt.clientX, evt.clientY);
-                if (selection && range) {
-                    selection.setBaseAndExtent(
-                        range.offsetNode,
-                        range.offset,
-                        range.offsetNode,
-                        range.offset
-                    );
-                }
-            }
+  handlePaste(evt) {
+    const clipboard = evt.clipboardData || window.clipboardData;
 
+    // IE 11 is .files other browsers are .items
+    if (clipboard && (clipboard.items || clipboard.files)) {
+      const items = clipboard.items || clipboard.files;
+      const IMAGE_MIME_REGEX = /^image\/(jpe?g|gif|png|svg|webp)$/i;
+
+      for (let i = 0; i < items.length; i++) {
+        if (IMAGE_MIME_REGEX.test(items[i].type)) {
+          const file = items[i].getAsFile ? items[i].getAsFile() : items[i];
+
+          if (file) {
             this.quill.focus();
             this.range = this.quill.getSelection();
-            let file = evt.dataTransfer.files[0];
-
+            evt.preventDefault();
             setTimeout(() => {
-                this.quill.focus();
-                this.range = this.quill.getSelection();
-                this.readAndUploadFile(file);
+              this.quill.focus();
+              this.range = this.quill.getSelection();
+              this.readAndUploadFile(file);
             }, 0);
+          }
         }
+      }
     }
+  }
 
-    handlePaste(evt) {
-        let clipboard = evt.clipboardData || window.clipboardData;
+  readAndUploadFile(file) {
+    let isUploadReject = false;
 
-        // IE 11 is .files other browsers are .items
-        if (clipboard && (clipboard.items || clipboard.files)) {
-            let items = clipboard.items || clipboard.files;
-            const IMAGE_MIME_REGEX = /^image\/(jpe?g|gif|png|svg|webp)$/i;
+    const fileReader = new FileReader();
 
-            for (let i = 0; i < items.length; i++) {
-                if (IMAGE_MIME_REGEX.test(items[i].type)) {
-                    let file = items[i].getAsFile ? items[i].getAsFile() : items[i];
+    let promiseUpload = null;
 
-                    if (file) {
-                        this.quill.focus();
-                        this.range = this.quill.getSelection();
-                        evt.preventDefault();
-                        setTimeout(() => {
-                            this.quill.focus();
-                            this.range = this.quill.getSelection();
-                            this.readAndUploadFile(file);
-                        }, 0);
-                    }
-                }
-            }
+    fileReader.addEventListener(
+      'load',
+      () => {
+        if (!isUploadReject) {
+          const base64ImageSrc = fileReader.result;
+          this.insertBase64Image(base64ImageSrc, promiseUpload);
         }
+      },
+      false,
+    );
+
+    if (file) {
+      fileReader.readAsDataURL(file);
     }
 
-    readAndUploadFile(file) {
-        let isUploadReject = false;
+    promiseUpload = this.options.upload(file);
 
-        const fileReader = new FileReader();
+    promiseUpload.then(
+      (imageUrl) => {
+        this.insertToEditor(imageUrl, promiseUpload);
+      },
+      (error) => {
+        isUploadReject = true;
+        this.removeBase64Image(promiseUpload);
+        console.warn(error);
+      },
+    );
+  }
 
-        fileReader.addEventListener(
-            "load",
-            () => {
-                if (!isUploadReject) {
-                    let base64ImageSrc = fileReader.result;
-                    this.insertBase64Image(base64ImageSrc);
-                }
-            },
-            false
-        );
+  fileChanged() {
+    const file = this.fileHolder.files[0];
+    this.readAndUploadFile(file);
+  }
 
-        if (file) {
-            fileReader.readAsDataURL(file);
-        }
+  insertBase64Image(url, promiseUpload) {
+    const { range } = this;
 
-        this.options.upload(file).then(
-            (imageUrl) => {
-                this.insertToEditor(imageUrl);
-            },
-            (error) => {
-                isUploadReject = true;
-                this.removeBase64Image();
-                console.warn(error);
-            }
-        );
+    this.placeholderDelta = this.quill.insertEmbed(
+      range.index,
+      LoadingImage.blotName,
+      `${url}`,
+      'user',
+    );
+
+    const leaf = this.quill.getLeaf(range.index)[0];
+
+    this.placeholderLeafs.set(promiseUpload, leaf.next || leaf);
+
+    range.index += 1;
+
+    this.quill.setSelection(range, 'user');
+  }
+
+  insertToEditor(url, promiseUpload) {
+    const leaf = this.placeholderLeafs.get(promiseUpload);
+
+    if (!leaf) {
+      return;
     }
 
-    fileChanged() {
-        const file = this.fileHolder.files[0];
-        this.readAndUploadFile(file);
+    const imageIndex = this.quill.getIndex(leaf);
+
+    const lengthToDelete = this.calculatePlaceholderInsertLength();
+
+    const newRange = this.quill.getSelection();
+
+    // Delete the placeholder image
+    this.quill.deleteText(imageIndex, lengthToDelete, 'user');
+    // Insert the server saved image
+    this.quill.insertEmbed(imageIndex, 'image', `${url}`, 'user');
+
+    if (newRange.index === imageIndex + 1) {
+      this.quill.setSelection(newRange.index + 1, 'user');
     }
 
-    insertBase64Image(url) {
-        const range = this.range;
+    this.placeholderLeafs.delete(promiseUpload);
+  }
 
-        this.quill.insertEmbed(range.index, 'break', true, 'user');
+  // The length of the insert delta from insertBase64Image can vary depending on what part of the line the insert occurs
+  calculatePlaceholderInsertLength() {
+    return this.placeholderDelta.ops.reduce((accumulator, deltaOperation) => {
+      if (deltaOperation.hasOwnProperty('insert')) { accumulator++; }
 
-        range.index++;
+      return accumulator;
+    }, 0);
+  }
 
-        this.placeholderDelta = this.quill.insertEmbed(
-            range.index,
-            LoadingImage.blotName,
-            `${url}`,
-            "user"
-        );
+  removeBase64Image(promiseUpload) {
+    const leaf = this.placeholderLeafs.get(promiseUpload);
 
-        this.placeholderLeaf = this.quill.getLeaf(range.index);
-
-        const newRange = this.quill.getSelection();
-
-        newRange.index += 3;
-
-        this.quill.insertText(newRange.index, '\n', 'user');
-
-        this.quill.setSelection(newRange, 'user');
+    if (!leaf) {
+      return;
     }
 
-    insertToEditor(url) {
-        const imageIndex = this.quill.getIndex(this.placeholderLeaf[0]) + this.placeholderLeaf[1];
+    const imageIndex = this.quill.getIndex(leaf);
 
-        const lengthToDelete = this.calculatePlaceholderInsertLength();
+    const lengthToDelete = this.calculatePlaceholderInsertLength();
 
-        // Delete the placeholder image
-        this.quill.deleteText(imageIndex, lengthToDelete, "user");
-        // Insert the server saved image
-        this.quill.insertEmbed(imageIndex, "image", `${url}`, "user");
-    }
+    this.quill.deleteText(imageIndex, lengthToDelete, 'user');
 
-    // The length of the insert delta from insertBase64Image can vary depending on what part of the line the insert occurs
-    calculatePlaceholderInsertLength() {
-        return this.placeholderDelta.ops.reduce((accumulator, deltaOperation) => {
-            if (deltaOperation.hasOwnProperty('insert'))
-                accumulator++;
-
-            return accumulator;
-        }, 0);
-    }
-
-    removeBase64Image() {
-        const range = this.range;
-        const lengthToDelete = this.calculatePlaceholderInsertLength();
-
-        this.quill.deleteText(range.index, lengthToDelete, "user");
-    }
+    this.placeholderLeafs.delete(promiseUpload);
+  }
 }
 
 window.ImageUploader = ImageUploader;
